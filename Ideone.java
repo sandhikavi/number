47111ce0e198d33a2764f643cd41f18351227687
sandhikavi <sandhyav.velusamy@gmail.com>
2016-08-16 18:12:19 +0530

    Update and rename .gitignore to Ideone.java

---
+++ Ideone.java
@@ -0,0 +1,41 @@
+
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+
+class Ideone
+{
+	
+public static void main (String[] args) throws java.lang.Exception
+
+	{
+
+int i=10;		
+f(i>0)
+{
+
+System.out.println("the number is positive");
+
+}
+else if(i<0)
+{
+System.out.println("the number is negative");
+
+}
+
+else
+
+{
+
+System.out.println("the number is zero");
+
+}	
+}
+
+}
